62494ab0acc4c84828a226e7f962c9de272d505c
Richard Michael Eason <rme15a@acu.edu>
2020-05-13 23:46:33 -0500

    pasted downloaded moment.js code, also added scripts.js file

---
+++ script.js
@@ -0,0 +1,6 @@
+var a = moment().format('LLLL');
+console.log(a);
+var b = moment().format('MMMM Do [the great year of YYYY');
+console.log(b);
+var c = moment().format('MM DD YYYY');
+console.log(c)
